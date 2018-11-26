97551904cc0339837a30e12cc14f43881fe0abd0
imrok le bleu <romaric.ruga@gmail.com>
2018-11-26 22:14:49 +0100

    lang/plant/params route

---
+++ functions/server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const routes = require('./routes')
+const LANGS = require('../utils/refs/langs')
+
+const app = express()
+const langRouter = express.Router()
+
+// 1. fill the lang router with every routes
+Object.keys(routes).forEach(name => langRouter.route(`/${ name }*`).get(routes[name]))
+
+// 2. associate the lang router to every lang prefix
+LANGS.forEach(lang => app.use('/' + lang.toLowerCase(), langRouter))
+
+module.exports = app
