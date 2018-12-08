4d6d0fa8cc192e241cd4af6fc8d61469816615c6
imrok le bleu <romaric.ruga@gmail.com>
2018-12-08 08:47:44 +0100

    ajout login gate

--- functions/client/modules/contributor.js
+++ functions/client/modules/contributor.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import firebase from 'firebase/app'
+import firebaseConfig from '../../firebase.config'
 import { render } from 'react-dom'
 import ImageConverter from '../../components/ImageConverter'
-import PlantAdder from '../../components/PlantAdder';
+import PlantAdder from '../../components/PlantAdder'
+import Login from '../../components/Login';
+
+firebase.initializeApp(firebaseConfig)
 
 class Contributor extends React.Component {
   constructor() {
     super()
     this.state = {
-      mode: 'image-converter'
+      mode: 'image-converter',
+      user: null
     }
   }
 
@@ -15,35 +21,48 @@ class Contributor extends React.Component {
     this.setState({ mode })
   }
 
+  updateUser(user) {
+    if (user) {
+      this.setState({ user })
+    } else {
+      this.setState({
+        user: null
+      })
+    }
+  }
+
   render() {
-    const { mode } = this.state
+    const { mode, user } = this.state
     return (
-      <React.Fragment>
-        <div id='menu' className='container'>
-          <div className='row'>
-            <button type='button'
-                className={ `btn btn-primary col-md-3${ mode === 'image-converter' ? ' active' : '' }` }
-                onClick={ () => this.handleModeChange('image-converter') }
-                disabled={ mode === 'image-converter' }>
-              image converter</button>
-            <button type='button'
-                className={ `btn btn-primary col-md-3${ mode === 'plant-adder' ? ' active' : '' }` }
-                onClick={ () => this.handleModeChange('plant-adder') }
-                disabled={ mode === 'plant-adder' }>
-              plant adder</button>
+      <Login user={ user }
+          updateUser={ this.updateUser.bind(this) }>
+        <div id='panel'>
+          <div id='menu' className='container'>
+            <div className='row'>
+              <button type='button'
+                  className={ `btn btn-primary col-md-3${ mode === 'image-converter' ? ' active' : '' }` }
+                  onClick={ () => this.handleModeChange('image-converter') }
+                  disabled={ mode === 'image-converter' }>
+                image converter</button>
+              <button type='button'
+                  className={ `btn btn-primary col-md-3${ mode === 'plant-adder' ? ' active' : '' }` }
+                  onClick={ () => this.handleModeChange('plant-adder') }
+                  disabled={ mode === 'plant-adder' }>
+                plant adder</button>
+            </div>
           </div>
+          {
+            mode === 'image-converter' && (
+              <ImageConverter/>
+            )
+          }
+          {
+            mode === 'plant-adder' && (
+              <PlantAdder/>
+            )
+          }
         </div>
-        {
-          mode === 'image-converter' && (
-            <ImageConverter/>
-          )
-        }
-        {
-          mode === 'plant-adder' && (
-            <PlantAdder/>
-          )
-        }
-      </React.Fragment>
+      </Login>
     )
   }
 }
---
+++ functions/firebase.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyBcuTro6kJSIEtzvw_reBL4GNRv0fUr6a0",
+  authDomain: "perma-data-3.firebaseapp.com",
+  databaseURL: "https://perma-data-3.firebaseio.com",
+  projectId: "perma-data-3",
+  storageBucket: "perma-data-3.appspot.com",
+  messagingSenderId: "642314046198"
+}
