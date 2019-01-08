570224e2a1fe4faea856d32564b8b4c89df607bb
imrok le bleu <romaric.ruga@gmail.com>
2019-01-08 15:59:13 +0100

    login: info, recovery, UI polishing

--- functions/components/Login/Login.js
+++ functions/components/Login/Login.js
@@ -8,7 +8,6 @@ export default class extends React.Component {
   constructor() {
     super()
     this.state = {
-      user: null,
       email: '',
       password: '',
       mode: 'login',
@@ -23,30 +22,40 @@ export default class extends React.Component {
   }
   
   handleEmailChange(email) {
-    this.setState({ email })
+    this.setState({
+      email,
+      info: null
+    })
   }
 
   handlePasswordChange(password) {
-    this.setState({ password })
+    this.setState({
+      password,
+      info: null
+    })
   }
 
   signIn() {
     const { email, password } = this.state
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => console.log(user))
-      .catch(err => console.log(err))
+      .then(() => this.setState({ info: null }))
+      .catch(err => this.setState({ info: err.message }))
   }
 
   signUp() {
     const { email, password } = this.state
     firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(user => console.log(user))
-      .catch(err => console.log(err))
+      .then(() => this.setState({ info: null }))
+      .catch(err => this.setState({
+        info: err.message
+      }))
   }
 
   signOut() {
     firebase.auth().signOut()
-      .then(() => console.log(null))
+      .then(() => this.setState({
+        info: 'You are now disconnected'
+      }))
   }
 
   switchToLogin() {
@@ -61,20 +70,20 @@ export default class extends React.Component {
     })
   }
 
-  resetPassword(resetPasswordSuccess) {
+  resetPassword() {
     const { email } = this.state
     firebase.auth().sendPasswordResetEmail(email)
       .then(() => this.setState({
-        info: resetPasswordSuccess
+        info: 'The password recovery mail has been sent'
       }))
       .catch(err => this.setState({
-        info: err
+        info: err.message
       }))
   }
 
   render() {
     const { user, children } = this.props
-    const { mode, email, password } = this.state
+    const { mode, email, password, info } = this.state
     return user ? 
       user && (
         <React.Fragment>
@@ -88,14 +97,23 @@ export default class extends React.Component {
           <div className='col-md-6 offset-md-3'>
             <label>email</label>
             <input type='email' className='form-control' value={ email }
-                onChange={ e => this.handleEmailChange(e.target.value) }/>
+                onChange={ e => this.handleEmailChange(e.target.value) }
+                onKeyPress={ e => (mode !== 'login'
+                  && e.charCode === 13) &&  this.resetPassword() }/>
             {
               mode === 'login' && (
                 <React.Fragment>
                   <label>password</label>
                   <input type='password' className='form-control' value={ password }
-                      onChange={ e => this.handlePasswordChange(e.target.value) }/>
+                      onChange={ e => this.handlePasswordChange(e.target.value) }
+                      onKeyPress={ e => e.charCode === 13 && this.signIn() }/>
                 </React.Fragment>
+              )
+            }
+            {
+              info && (
+                <div id='login-form__info'>
+                  { info }</div>
               )
             }
             <div id='login-form__menu'>
@@ -103,26 +121,29 @@ export default class extends React.Component {
                 mode == 'login' ? (
                   <React.Fragment>
                     <button type='button'
-                        className='btn btn-outline-secondary col-lg-4 col-md-6'
+                        className='btn btn-x btn-outline-secondary col-lg-4 col-md-6'
                         onClick={ () => this.switchToResetPassword() }>
                       reset password</button>
                     <button type='button'
-                        className='btn btn-outline-secondary col-lg-4 col-md-6'
-                        onClick={ () => this.signIn() }>
-                      sign in</button>
-                    <button type='button'
-                        className='btn btn-primary col-lg-4'
+                        className='btn btn-x btn-outline-secondary col-lg-4'
                         onClick={ () => this.signUp() }>
                       sign up</button>
+                    <button type='button'
+                        className='btn btn-x btn-primary col-lg-4 col-md-6'
+                        onClick={ () => this.signIn() }
+                        disabled={ email === '' || password === '' }>
+                      sign in</button>
                   </React.Fragment>
                 ) : (
                   <React.Fragment>
                     <button type='button'
-                        className='btn btn-outline-secondary col-md-6'
+                        className='btn btn-x btn-outline-secondary col-md-6'
                         onClick={ () => this.switchToLogin() }>
                       back to login</button>
-                    <button type='button' className='btn btn-primary col-md-6'
-                        onClick={ () => this.resetPassword(resetPasswordSuccess) }>
+                    <button type='button'
+                        className='btn btn-x btn-primary col-md-6'
+                        onClick={ () => this.resetPassword() }
+                        disabled={ email === '' }>
                       send recovery</button>
                   </React.Fragment>
                 )
