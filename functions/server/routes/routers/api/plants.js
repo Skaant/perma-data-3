8ac17824b283c02ed97506286e26d7296c6ed193
imrok le bleu <romaric.ruga@gmail.com>
2019-01-22 19:14:59 +0100

    fix plant add api

--- functions/server/routes/routers/api/plants.js
+++ functions/server/routes/routers/api/plants.js
@@ -2,7 +2,7 @@ module.exports = {
   add: (req, res) => {
     const plant = JSON.parse(req.body)
     const newPlant = Object.assign({}, plant, {
-      sup: global.db.collection('plants').doc(plant.sup)
+      sup: plant.rank === 'family' ? null : global.db.collection('plants').doc(plant.sup)
     })
     global.db.collection('plants').doc(plant.id).set(newPlant)
       .then(() => {
--- functions/server/routes/routers/contribute.js
+++ functions/server/routes/routers/contribute.js
@@ -1,8 +1,6 @@
 const extract = require('./contribute/extract')
-const plant = require('./contribute/plant')
 
 module.exports = router => {
   router.route('/extract').put(extract)
-  router.route('/plant').put(plant)
   return router
 }
